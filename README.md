041e3a73f6ba062f86b7ccb33956b5d497a8716c
chadday <emilyvanzandt@Emilys-MacBook-Pro.local>
2018-07-21 13:59:51 -0400

    Just adding some new stuff

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gtown
 Teaching materials for Georgetown data journalism
+
+A place to put some of the tutorials we'll be using for basic python and webscraping in class. 
+
---
+++ test.py
@@ -0,0 +1,22 @@
+
+'''
+Introduction to python and basic web scraping
+'''
+import csv
+import json
+import bs4 
+
+
+list = ['hello', 'world']
+
+for i in list: 
+    print(i)
+
+
+
+'''
+
+'''
+
+url = 'http://www.dcd.uscourts.gov/unsealed-opinions-sealed-cases'
+
